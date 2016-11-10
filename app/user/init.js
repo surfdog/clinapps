bce59837eda0fdce21c044d7807bed5b6a0384bb
Marc Tibbitts <marc@shiphawk.com>
2016-11-09 16:54:07 -0800

    initial commit

---
+++ app/user/init.js
@@ -0,0 +1,39 @@
+const rp = require('request-promise')
+const storage = require('node-persist')
+
+function initUser (app) {
+  storage.initSync();
+  app.get('/', renderWelcome)
+  app.get('/profile', renderProfile)
+  app.post('/login', (req, res) => {  
+    rp({
+      method: 'POST',
+      uri: 'http://54.153.108.164/api/v1/tokens',
+      body: {
+        email: req.body.email,
+        password: req.body.password
+      },
+      json: true
+      })
+      .then((data) => {
+        storage.setItem('userToken', data.id_token)
+        renderProfile(req, res)
+      })
+      .catch((err) => {
+        console.log(err)
+        renderWelcome(req, res)
+      })
+  })
+}
+
+function renderWelcome (req, res) {
+  res.render('user/welcome')
+}
+
+function renderProfile (req, res) {
+  res.render('user/profile', {
+    email: req.body.email
+  })
+}
+
+module.exports = initUser
